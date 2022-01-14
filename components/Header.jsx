b3787a9a6f7f1f02eb7df92a0868e13cb9091e6e
FlorAbril <florenciasoto801@gmail.com>
2022-01-14 08:46:58 -0300

    Improve responsiveness

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,45 +4,54 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="
-        h-128 mb-16
-        flex flex-wrap justify-center content-center
-        md:justify-evenly md:items-center
+        h-128 mb-16 mt-16
+        flex flex-wrap justify-center content-start
+        sm:justify-evenly sm:items-center
         bg-layered-waves
     ">
-      <div className="flex-none relative w-40 h-40 md:w-52 md:h-52 lg:w-64 lg:h-64">
+      <div className="flex-none w-1/2 sm:w-1/4 xl:w-1/5 max-w-xs">
           <Image
             src="/profile.jpg"
             alt="profile"
-            layout="fill"
+            width={1}
+            height={1}
+            layout="responsive"
+            objectFit="cover"
             className="rounded-full"
           />
       </div>
       <div className="p-4 h-fit">
-        <h1 className="text-xl text-center md:text-4xl md:text-left">
+        <h1 className="text-xl text-center sm:text-3xl md:text-4xl sm:text-left">
           Hi, I&apos;m Florencia Soto
         </h1>
-        <h2 className="text-sm text-center md:text-lg">
+        <h2 className="text-sm text-center sm:text-base md:text-lg">
           Frontend Developer based in Buenos Aires, AR.
         </h2>
-        <div className="flex justify-center gap-3 pt-3 md:justify-start items-center">
+        <div className="flex justify-center gap-3 pt-3 sm:justify-start items-center text-sm sm:text-base md:text-lg">
           <a href={"https://www.linkedin.com/in/florabrilsoto"} target={"_blank"}>
             <div className="flex items-center gap-1 cursor-pointer">
-              <Image
-                  src="/linkedin.svg"
-                  width={28}
-                  height={28}
-              />
+              <div className="w-4 md:w-6">
+                <Image
+                    src="/linkedin.svg"
+                    width={1}
+                    height={1}
+                    layout="responsive"
+                />
+              </div>
               <span className="transition duration-500 hover:underline hover:text-light-blue">LinkeIn</span>
             </div>
           </a>
-          <div className="flex items-center gap-1 cursor-pointer" target={"_blank"}>
-            <Image
-              src="/github.svg"
-              height={28}
-              width={28}
-            />
+          <a className="flex items-center gap-1 cursor-pointer" target={"_blank"}>
+            <div className="w-4 md:w-6">
+              <Image
+                src="/github.svg"
+                width={1}
+                height={1}
+                layout="responsive"
+              />
+            </div>
             <a className="transition duration-500 hover:underline hover:text-light-blue">Github</a>
-          </div>
+          </a>
         </div>
       </div>
     </div>
