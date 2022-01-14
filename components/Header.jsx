5c3fcce1205860381d22a1551536115f5a2b01e2
FlorAbril <florenciasoto801@gmail.com>
2022-01-13 22:54:41 -0300

    Change Header appearance, add background

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,49 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="mt-8 mb-16 flex flex-wrap justify-center md:justify-evenly md:items-center md:mt-12 md:mb-32">
+    <div className="
+        h-128 mb-16
+        flex flex-wrap justify-center content-center
+        md:justify-evenly md:items-center
+        bg-layered-waves
+    ">
       <div className="flex-none relative w-40 h-40 md:w-52 md:h-52 lg:w-64 lg:h-64">
-        <Image
-          src="/profile.jpg"
-          alt="profile"
-          layout="fill"
-          className="rounded-full"
-        />
+          <Image
+            src="/profile.jpg"
+            alt="profile"
+            layout="fill"
+            className="rounded-full"
+          />
       </div>
       <div className="p-4 h-fit">
         <h1 className="text-xl text-center md:text-4xl md:text-left">
           Hi, I&apos;m Florencia Soto
         </h1>
-        <h2 className="text-sm md:text-lg">
+        <h2 className="text-sm text-center md:text-lg">
           Frontend Developer based in Buenos Aires, AR.
         </h2>
+        <div className="flex justify-center gap-3 pt-3 md:justify-start items-center">
+          <a href={"https://www.linkedin.com/in/florabrilsoto"} target={"_blank"}>
+            <div className="flex items-center gap-1 cursor-pointer">
+              <Image
+                  src="/linkedin.svg"
+                  width={28}
+                  height={28}
+              />
+              <span className="transition duration-500 hover:underline hover:text-light-blue">LinkeIn</span>
+            </div>
+          </a>
+          <div className="flex items-center gap-1 cursor-pointer" target={"_blank"}>
+            <Image
+              src="/github.svg"
+              height={28}
+              width={28}
+            />
+            <a className="transition duration-500 hover:underline hover:text-light-blue">Github</a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
     extend: {
       backgroundImage: {
         'profile': "url('/profile.jpg')",
-      }
+        'stacked-waves': "url('/stacked-waves.svg')",
+        'blob-scene': "url('/blob-scene.svg')",
+        'layered-waves': "url('/layered-waves.svg')",
+        'circles': "url('/circles.svg')",
+      },
+      height: {
+        '128': '32rem',
+      },
+      colors: {
+        'dark-blue': '#0056d6',
+        'light-blue': '#00b2ff',
+      },
     },
   },
   plugins: [],
